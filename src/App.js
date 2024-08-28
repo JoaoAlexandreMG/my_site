2a8f0bd628b81bec68179e90dc5fe00cb9730c75
João Alexandre <j.sa@ufu.br>
2024-08-28 20:29:48 -0300

    retirado hrefs

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from 'react';
-import HomePage from './HomePage';
-import ProjectsPage from './ProjectsPage';
+import React, { useState } from "react";
+import HomePage from "./HomePage";
+import ProjectsPage from "./ProjectsPage";
 
 function App() {
-    const [currentPage, setCurrentPage] = useState('home');
+  const [currentPage, setCurrentPage] = useState("home");
 
-    const renderPage = () => {
-        switch (currentPage) {
-            case 'home':
-                return <HomePage />;
-            case 'projetos':
-                return <ProjectsPage />;
-            default:
-                return <HomePage />;
-        }
-    };
+  const renderPage = () => {
+    switch (currentPage) {
+      case "home":
+        return <HomePage />;
+      case "projetos":
+        return <ProjectsPage />;
+      default:
+        return <HomePage />;
+    }
+  };
 
-    return (
-        <div>
-            <nav>
-                <div className="logo">
-                    <i className="fa fa-code"></i>
-                </div>
-                <div className="links">
-                    <a href="#" className={currentPage === 'home' ? 'active' : ''} onClick={() => setCurrentPage('home')}>Sobre</a>
-                    <a href="#" className={currentPage === 'projetos' ? 'active' : ''} onClick={() => setCurrentPage('projetos')}>Projetos</a>                </div>
-            </nav>
-            {renderPage()}
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <i className="fa fa-code"></i>
         </div>
-    );
+        <div className="links">
+          <a
+            className={currentPage === "home" ? "active" : ""}
+            onClick={() => setCurrentPage("home")}
+          >
+            Sobre
+          </a>
+          <a
+            className={currentPage === "projetos" ? "active" : ""}
+            onClick={() => setCurrentPage("projetos")}
+          >
+            Projetos
+          </a>{" "}
+        </div>
+      </nav>
+      {renderPage()}
+    </div>
+  );
 }
 
 export default App;
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,93 +1,101 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React from "react";
 
 function HomePage() {
-    return (
-        <div>
-            <div className="sobre">
-                <div className="left">
-                    <h3>Estudante de Engenharia de Computação - UFU</h3>
-                    <div className="info">
-                        <h2>Meu nome é João Alexandre</h2>
-                        <p>
-                            Estou em busca de uma oportunidade de estágio na área de tecnologia,
-                            com foco no desenvolvimento de software back-end e em áreas correlacionadas.
-                        </p>
-                        <div className="buttons">
-                            <button className="hire">
-                                Contrate-me <i className="fa-solid fa-plus"></i>
-                            </button>
-                            <button className="email">
-                                Copiar email <i className="fa-regular fa-copy"></i>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div className="right">
-                    <a href="#" className="online"><i className="fa fa-circle"> Disponivel para trabalho</i></a>
-                    <img src="profile.jpg" alt="Profile" />
-                </div>
+  return (
+    <div>
+      <div className="sobre">
+        <div className="left">
+          <h3>Estudante de Engenharia de Computação - UFU</h3>
+          <div className="info">
+            <h2>Meu nome é João Alexandre</h2>
+            <p>
+              Estou em busca de uma oportunidade de estágio na área de
+              tecnologia, com foco no desenvolvimento de software back-end e em
+              áreas correlacionadas.
+            </p>
+            <div className="buttons">
+              <button className="hire">
+                Contrate-me <i className="fa-solid fa-plus"></i>
+              </button>
+              <button className="email">
+                Copiar email <i className="fa-regular fa-copy"></i>
+              </button>
             </div>
-
-            <div className="works">
-                <div className="right">
-                    <div className="item">
-                        <a>Descrição sobre mim</a>
-                        <p>
-                            Entusiasta da tecnologia com conhecimento intermediário em Python
-                            (incluindo Selenium, Flask, redes neurais e automação) e
-                            conhecimentos básicos em HTML5, CSS, JavaScript, C++, C e SQL.
-                        </p>
-                    </div>
-                    <div className="item">
-                        <a>Formação</a>
-                        <ul>
-                            <li>
-                                Técnico em Automação Industrial - NÃO CONCLUÍDO
-                            </li>
-                            <li>
-                                Bacharelado em Engenharia de Computação -
-                                Universidade Federal de Uberlândia (Andamento -
-                                2026)
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="item">
-                        <a>Cursos Adicionais</a>
-                        <ul>
-                            <li>
-                                Office completo - 120h
-                            </li>
-                            <li>
-                                Programação de CLPS - 60h
-                            </li>
-                            <li>
-                                Introdução a Rede de Computadores - 15h
-                            </li>
-                            <li>
-                                Gestão do tempo e produtividade - 2h
-                            </li>
-                            <li>
-                                Fundamentos de TI- Hardware e Software - 15h
-                            </li>
-                            <li>
-                                Python básico ao avançado.
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <footer>
-                <div className="socials">
-                    <a target='_blank' href="https://github.com/JoaoAlexandreMG" rel="noreferrer">Github</a>
-                    <a target='_blank' href="https://www.instagram.com/joaoa_sa/" rel="noreferrer">Instagram</a>
-                    <a target='_blank' href="https://www.linkedin.com/in/joaosa2004/" rel="noreferrer">LinkedIn</a>
-                </div>
-                <a className="online"><i className="fas fa-circle"></i> Disponível para trabalho</a>
-            </footer>
+          </div>
         </div>
-    );
+        <div className="right">
+          <a className="online">
+            <i className="fa fa-circle"> Disponivel para trabalho</i>
+          </a>
+          <img src="profile.jpg" alt="Profile" />
+        </div>
+      </div>
+
+      <div className="works">
+        <div className="right">
+          <div className="item">
+            <a>Descrição sobre mim</a>
+            <p>
+              Entusiasta da tecnologia com conhecimento intermediário em Python
+              (incluindo Selenium, Flask, redes neurais e automação) e
+              conhecimentos básicos em HTML5, CSS, JavaScript, C++, C e SQL.
+            </p>
+          </div>
+          <div className="item">
+            <a>Formação</a>
+            <ul>
+              <li>Técnico em Automação Industrial - NÃO CONCLUÍDO</li>
+              <li>
+                Bacharelado em Engenharia de Computação - Universidade Federal
+                de Uberlândia (Andamento - 2026)
+              </li>
+            </ul>
+          </div>
+          <div className="item">
+            <a>Cursos Adicionais</a>
+            <ul>
+              <li>Office completo - 120h</li>
+              <li>Programação de CLPS - 60h</li>
+              <li>Introdução a Rede de Computadores - 15h</li>
+              <li>Gestão do tempo e produtividade - 2h</li>
+              <li>Fundamentos de TI- Hardware e Software - 15h</li>
+              <li>Python básico ao avançado.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <footer>
+        <div className="socials">
+          <a
+            target="_blank"
+            href="https://github.com/JoaoAlexandreMG"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          <a
+            target="_blank"
+            href="https://www.instagram.com/joaoa_sa/"
+            rel="noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/joaosa2004/"
+            rel="noreferrer"
+          >
+            LinkedIn
+          </a>
+        </div>
+        <a className="online">
+          <i className="fas fa-circle"></i> Disponível para trabalho
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default HomePage;
